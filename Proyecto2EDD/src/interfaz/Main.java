cc6ff541c3c6225b065cd95ea3fd07a658672262
AndreRoj <130945779+AndreRoj@users.noreply.github.com>
2024-03-10 20:59:31 -0400

    Creación de la interfaz del programa
    Solo la parte visual sin código detrás aún

---
+++ Proyecto2EDD/src/interfaz/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfaz;
+
+/**
+ *
+ * @author pjroj
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Inicio inicio = new Inicio();
+        inicio.setVisible(true);
+    }
+    
+}
