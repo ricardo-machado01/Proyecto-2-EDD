f6ba1ce44e99f6833773900899e095c5f6115bfd
ricardo-machado01 <ricardo.machado@correo.unimet.edu.ve>
2024-03-26 19:30:48 -0400

    Ajustando la clase Main :bug:

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,11 +1,27 @@
 package Main;
 
+import Interfaces.*;
 
+/**
+ * Clase Principal del Proyecto.
+ * @author Nicolas Planas, Ricardo Machado, Andrea Rojas, Luciano Ruiz.
+ */
 public class Main {
 
-
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+        
+        // Cargamos los hash tables.
+        Global.cvsReaderHash();
+
+        // Cargamos los datos y creamos los arboles.
+        Global.cvsReaderReservations();
+        Global.cvsReaderRoomsHistory();
+        
+        // Definimos que habitaciones no están disponibles.
+        Global.getClients().notAvailableRooms();
+
+        // Creamos la ventana principal.
+        W w = new W();
+        w.setVisible(true);
+        }
 }
