10842d6c1873ff992acb98642074db12b82ef2e6
ricardo-machado01 <ricardo.machado@correo.unimet.edu.ve>
2024-03-27 19:35:51 -0400

    Añadiendo función checkIn a la clase HashTable :sparkles:

--- src/Hashtable/CHashtable.java
+++ src/Hashtable/CHashtable.java
@@ -4,8 +4,10 @@ import Classes.Class_Client;
 import Classes.Class_Reservation;
 import Classes.Class_Room;
 import Main.Global;
+import Nodes.Node_Client;
+import Nodes.Node_Reservation;
 import Nodes.Node_Reservation_List;
-
+import Nodes.Node_Room;
 
 /**
  * Clase para crear el Hash Table.
@@ -50,56 +52,91 @@ public class CHashtable {
      */
     public void addClientTable(Class_Client client, String arrival_date){
         int index = getHashIndex(client.getName(), client.getLastname());
+        
         if (clientTable[index] != null) {
             Node_Reservation_List currentNode = clientTable[index];
              
             while (currentNode.getNext() != null) {
                 currentNode = currentNode.getNext();
             }
+
             currentNode.setNext(new Node_Reservation_List(new Class_Reservation(client, null, arrival_date, null)));
         } else {
             clientTable[index] = new Node_Reservation_List(new Class_Reservation(client, null, arrival_date, null));
         }
     }
-
+    
     /**
-     * Este método busca al cliente en base a su nombre y apellido.
-     * @param name Nombre del Cliente.
-     * @param lastname Apellido del Cliente.
-     * @return Retorna la información del cliente.
+     * Método para eliminar al cliente del Hash Table.
+     * @param id
+     * @return 
      */
-    public Class_Reservation searchClient(String name, String lastname){
-        int index = getHashIndex(name, lastname);
-        Node_Reservation_List currentNode = clientTable[index];
-        Class_Reservation searchResult =  null;
-        //String searchResult = "";
+    public Class_Client checkInClient(int id) {
+        Node_Reservation reservation = Global.getReservations().search(id, Global.getReservations().getRoot());
+        String roomType = reservation.getReservation().getRoom_type();
+
+        // BUSCAMOS LA HABITACIÓN QUE ESTÉ DISPONIBLE DEPENDIENDO DEL TIPO QUE DESEE EL CLIENTE.
+        Class_Room roomAvailable = Global.getRooms().searchRoomType(roomType, Global.getRooms().getRoot());
+        roomAvailable.setAvailable(false);
+
+        // AÑADIMOS EL CLIENTE AL REGISTRO DE ESTADO EN EL HASHTABLE.
+        Global.getClients().addClientTable(reservation.getReservation().getClient(), reservation.getReservation().getArrival());
         
-        while (currentNode != null) {
-            if (currentNode.getReservation().getClient().getName().equals(name) && currentNode.getReservation().getClient().getLastname().equals(lastname)) {
-                searchResult = currentNode.getReservation();
-                //searchResult += currentNode.getReservation().getClient().showInfoStatus() + "Llegada: " + currentNode.getReservation().getArrival();
-            } currentNode = currentNode.getNext();
-        }
-        return searchResult;
+        // ASIGNAMOS LA HABITACIÓN AL CLIENTE.
+        reservation.getReservation().getClient().setRoomNumber(Integer.toString(roomAvailable.getNumber()));
+
+        // EN EL ARBOL RESERVACIONES ELIMINAMOS LA RESERVACIÓN DEL CLIENTE.
+        Global.getReservations().delete(id, Global.getReservations().getRoot(), null);
+        
+        return reservation.getReservation().getClient();
     }
-        
+    
     /**
      * Método para eliminar al cliente del Hash Table.
      * @param name Nombre del Cliente.
      * @param lastname Apellido del Cliente.
+     * @return 
      */
-    public void checkOutClient(String name, String lastname) {
-        for (int i = 0; i < clientTable.length-1; i++) {
-            while(clientTable[i] != null){
-                if(clientTable[i].getReservation().getClient().getName().equals(name) && clientTable[i].getReservation().getClient().getName().equals(lastname)){
-                    clientTable[i] = null;
-                    break;
+    public Class_Client checkOutClient(String name, String lastname) {
+        
+        int index = getHashIndex(name, lastname);
+        Node_Reservation_List currentNode = clientTable[index];
+        Node_Reservation_List prevNode = clientTable[index];
+
+        if (clientTable[index] != null) {
+            if (currentNode.getNext() != null) {
+                while (currentNode != null) {
+                    if (currentNode.getReservation().getClient().getName().equals(name) && currentNode.getReservation().getClient().getLastname().equals(lastname)) {
+                        
+                        int room_number = Integer.parseInt(currentNode.getReservation().getClient().getRoomNumber());
+                        Node_Room room = Global.getRooms().search(room_number, Global.getRooms().getRoot());
+                        Node_Client client = new Node_Client(currentNode.getReservation().getClient());
+                        room.getRoom().getClientHistory().insertBegining(client);
+                        
+                        if (clientTable[index] == currentNode) {
+                            clientTable[index] = currentNode.getNext();
+                            break;
+                        } else {
+                            prevNode.setNext(currentNode.getNext());
+                            break; 
+                        }
+                    }
+                    prevNode = currentNode;
+                    currentNode = currentNode.getNext();
+                    }
+            } else if (currentNode.getReservation().getClient().getName().equals(name) && currentNode.getReservation().getClient().getLastname().equals(lastname)) {
+                
+                int room_number = Integer.parseInt(currentNode.getReservation().getClient().getRoomNumber());
+                Node_Room room = Global.getRooms().search(room_number, Global.getRooms().getRoot());
+                Node_Client client = new Node_Client(currentNode.getReservation().getClient());
+                room.getRoom().getClientHistory().insertBegining(client);
+
+                clientTable[index] = null;
                 }
-                clientTable[i] = clientTable[i].getNext();
             }
+            return currentNode.getReservation().getClient();
         }
-    }
-    
+
     /**
      * Método para identificar en el Arbol de habitaciones, las habitaciones OCUPADAS.
      */
@@ -116,22 +153,32 @@ public class CHashtable {
         }
     }
 
-    //MÉTODOS GET.
+    /**
+     * Este método busca al cliente en base a su nombre y apellido.
+     * @param name Nombre del Cliente.
+     * @param lastname Apellido del Cliente.
+     * @return Retorna la información del cliente.
+     */
+    public String searchClient(String name, String lastname){
+        
+        int index = getHashIndex(name, lastname);
+        Node_Reservation_List currentNode = clientTable[index];
+        String searchResult = "";
+        
+        while (currentNode != null) {
+            if (currentNode.getReservation().getClient().getName().equals(name) && currentNode.getReservation().getClient().getLastname().equals(lastname)) {
+                searchResult += currentNode.getReservation().getClient().showInfoStatus() + "Llegada: " + currentNode.getReservation().getArrival();
+            } currentNode = currentNode.getNext();
+        }
+        return searchResult;
+    }
+
+    // MÉTODOS GET.
     public Node_Reservation_List[] getClientTable() {
         return clientTable;
     }
-    
+
     public int getCapacity() {
         return capacity;
     }
-
-    //MÉTODOS SET.
-    public void setClientTable(Node_Reservation_List[] clientTable) {
-        this.clientTable = clientTable;
-    }
-
-    public void setCapacity(int capacity) {
-        this.capacity = capacity;
-    }
-
 }
